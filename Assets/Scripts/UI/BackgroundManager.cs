da871dc0b549b780ce8e6bc3021123bcaa333c25
Haley Requa <haleyrequa@HaleyResMacBook.hitronhub.home>
2017-02-17 22:56:43 -0800

    Background still in process. Position is a tiny bit off. Why?

--- Assets/Scripts/UI/BackgroundManager.cs
+++ Assets/Scripts/UI/BackgroundManager.cs
@@ -6,7 +6,7 @@ public class BackgroundManager : MonoBehaviour {
 
 	public Vector2 scrollSpeed;
 	public Vector2 direction;
-	public Vector2 input;
+	public Vector2 acceleration; 
 	public float tileSizeZ;
 	public float zdepth;
 	public float rotation;
@@ -20,25 +20,23 @@ public class BackgroundManager : MonoBehaviour {
 	{
 		startPosition = transform.position;
 		tileSizeZ = tileSizeZ * Mathf.Cos (Mathf.Deg2Rad * rotation);
-		zdepth = tileSizeZ * Mathf.Sin (Mathf.Deg2Rad * rotation);
+		zdepth = tileSizeZ * Mathf.Sin (Mathf.Deg2Rad * rotation) / 2f;
 	}
 
 	void Update ()
 	{
-		x = Mathf.Repeat(Time.time * direction.x * scrollSpeed.x + input.x, tileSizeZ);
-		y = Mathf.Repeat(Time.time * direction.y * scrollSpeed.y + input.y, tileSizeZ);
-		z = Mathf.Lerp (-zdepth/2, zdepth/2, y / tileSizeZ);
-		transform.position = new Vector3 (x, y, z);
+		x = Mathf.Repeat(Time.time * direction.x * scrollSpeed.x + acceleration.x, tileSizeZ);
+		y = Mathf.Repeat(Time.time * direction.y * scrollSpeed.y + acceleration.y, tileSizeZ);
+		z = Mathf.Lerp (0f, zdepth*2f, y / tileSizeZ);
+		transform.position = startPosition + new Vector3 (x, y, z);
 		//scrollSpeed.x -= (scrollSpeed.x > 0) ? 0.01f : 0f; // Decelerate
 		//scrollSpeed.y -= (scrollSpeed.y > 0) ? 0.01f : 0f; // Decelerate
 	}
 
 	public void Input(Vector2 keyInput) {
-		input += keyInput * .1f;
-		direction.x = keyInput.x > 0 ? 1 : direction.x;
-		direction.x = keyInput.x < 0 ? -1 : direction.x;
-		direction.y = keyInput.y > 0 ? 1 : direction.y;
-		direction.y = keyInput.y < 0 ? -1 : direction.y;
+		acceleration += keyInput * .1f;
+		direction.x = keyInput.x > 0 ? 1 : keyInput.x < 0 ? -1 : direction.x;
+		direction.y = keyInput.y > 0 ? 1 : keyInput.y < 0 ? -1 : direction.y;
 //		scrollSpeed += new Vector2(Mathf.Abs(keyInput.x),Mathf.Abs(keyInput.y)) * 0.1f;
 	}
 }
